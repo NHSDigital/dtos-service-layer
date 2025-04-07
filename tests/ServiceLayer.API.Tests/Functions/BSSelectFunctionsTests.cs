f9f0e5075038d0e55a78f88f14ab726ab7567952
alex-clayton-1 <alexander.clayton1@nhs.net>
2025-04-07 17:12:39 +0100

    tests: Refactored tests to improve maintainability

--- tests/ServiceLayer.API.Tests/Functions/BSSelectFunctionsTests.cs
+++ tests/ServiceLayer.API.Tests/Functions/BSSelectFunctionsTests.cs
@@ -1,4 +1,5 @@
-﻿using Azure;
+﻿using System.Dynamic;
+using Azure;
 using Azure.Messaging;
 using Azure.Messaging.EventGrid;
 using Microsoft.AspNetCore.Http;
@@ -16,25 +17,33 @@ public class BSSelectFunctionsTests
     private readonly Mock<EventGridPublisherClient> _mockEventGridPublisherClient = new();
     private readonly BSSelectFunctions _functions;
     private readonly SetupRequest _setupRequest = new();
+    private readonly dynamic _episode = new ExpandoObject();
+    public static TheoryData<string> RequiredPropertyNames =>
+    [
+        "episode_id",
+        "nhs_number",
+        "date_of_birth",
+        "first_given_name",
+        "family_name"
+    ];
 
     public BSSelectFunctionsTests()
     {
         _functions = new BSSelectFunctions(_logger.Object, _mockEventGridPublisherClient.Object);
+
+        // Configuring a valid episode
+        _episode.episode_id = "123";
+        _episode.nhs_number = "9990000000";
+        _episode.date_of_birth = "1970-01-01";
+        _episode.first_given_name = "Test";
+        _episode.family_name = "User";
     }
 
     [Fact]
     public async Task CreateEpisodeEvent_ShouldSendEventAndReturnOk_WhenRequestIsValid()
     {
         // Arrange
-        var episode = new
-        {
-            episode_id = "123",
-            nhs_number = "9990000000",
-            date_of_birth = "1970-01-01",
-            first_given_name = "Test",
-            family_name = "User",
-        };
-        var request = _setupRequest.CreateMockHttpRequest(episode);
+        var request = _setupRequest.CreateMockHttpRequest(_episode);
         var mockResponse = Mock.Of<Response>(r => r.IsError == false);
         _mockEventGridPublisherClient.Setup(x => x.SendEventAsync(It.IsAny<CloudEvent>(), It.IsAny<CancellationToken>())).ReturnsAsync(mockResponse);
 
@@ -61,99 +70,71 @@ public class BSSelectFunctionsTests
         _mockEventGridPublisherClient.Verify(x => x.SendEventAsync(It.IsAny<CloudEvent>(), It.IsAny<CancellationToken>()), Times.Never());
     }
 
-    [Fact]
-    public async Task CreateEpisodeEvent_ShouldReturnBadRequest_WhenEpisodeIdIsMissing()
+    [Theory]
+    [MemberData(nameof(RequiredPropertyNames))]
+    public async Task CreateEpisodeEvent_ShouldReturnBadRequest_WhenRequiredPropertyIsMissing(string propertyName)
     {
         // Arrange
-        var episode = new
-        {
-            nhs_number = "9990000000",
-            date_of_birth = "1970-01-01",
-            first_given_name = "Test",
-            family_name = "User",
-        };
-        var request = _setupRequest.CreateMockHttpRequest(episode);
+        ((IDictionary<string, object?>)_episode).Remove(propertyName);
+        var request = _setupRequest.CreateMockHttpRequest(_episode);
 
         // Act
         var response = await _functions.IngressEpisode(request);
 
         // Assert
         var result = Assert.IsType<BadRequestObjectResult>(response);
-        Assert.Equal("episode_id is required", result.Value);
+        Assert.Equal($"{propertyName} is required", result.Value);
         _mockEventGridPublisherClient.Verify(x => x.SendEventAsync(It.IsAny<CloudEvent>(), It.IsAny<CancellationToken>()), Times.Never());
     }
 
     [Theory]
-    [InlineData(null)]
-    [InlineData("")]
-    [InlineData(" ")]
-    public async Task CreateEpisodeEvent_ShouldReturnBadRequest_WhenEpisodeIdIsEmptyValue(string? episodeId)
+    [MemberData(nameof(RequiredPropertyNames))]
+    public async Task CreateEpisodeEvent_ShouldReturnBadRequest_WhenRequiredPropertyIsNull(string propertyName)
     {
         // Arrange
-        var episode = new
-        {
-            episode_id = episodeId,
-            nhs_number = "9990000000",
-            date_of_birth = "1970-01-01",
-            first_given_name = "Test",
-            family_name = "User",
-        };
-        var request = _setupRequest.CreateMockHttpRequest(episode);
+        ((IDictionary<string, object?>)_episode)[propertyName] = null;
+        var request = _setupRequest.CreateMockHttpRequest(_episode);
 
         // Act
         var response = await _functions.IngressEpisode(request);
 
         // Assert
         var result = Assert.IsType<BadRequestObjectResult>(response);
-        Assert.Equal("episode_id is required", result.Value);
-        _mockEventGridPublisherClient.Verify(x => x.SendEventAsync(It.IsAny<CloudEvent>(), It.IsAny<CancellationToken>()), Times.Never());
-    }
-
-    [Fact]
-    public async Task CreateEpisodeEvent_ShouldReturnBadRequest_WhenNhsNumberIsMissing()
-    {
-        // Arrange
-        var episode = new
-        {
-            episode_id = "123",
-            date_of_birth = "1970-01-01",
-            first_given_name = "Test",
-            family_name = "User",
-        };
-        var request = _setupRequest.CreateMockHttpRequest(episode);
-
-        // Act
-        var response = await _functions.IngressEpisode(request);
-
-        // Assert
-        var result = Assert.IsType<BadRequestObjectResult>(response);
-        Assert.Equal("nhs_number is required", result.Value);
+        Assert.Equal($"{propertyName} is required", result.Value);
         _mockEventGridPublisherClient.Verify(x => x.SendEventAsync(It.IsAny<CloudEvent>(), It.IsAny<CancellationToken>()), Times.Never());
     }
 
     [Theory]
-    [InlineData(null)]
-    [InlineData("")]
-    [InlineData(" ")]
-    public async Task CreateEpisodeEvent_ShouldReturnBadRequest_WhenNhsNumberIsEmptyValue(string? nhsNumber)
+    [MemberData(nameof(RequiredPropertyNames))]
+    public async Task CreateEpisodeEvent_ShouldReturnBadRequest_WhenRequiredPropertyIsEmptyString(string propertyName)
     {
         // Arrange
-        var episode = new
-        {
-            episode_id = "123",
-            nhs_number = nhsNumber,
-            date_of_birth = "1970-01-01",
-            first_given_name = "Test",
-            family_name = "User",
-        };
-        var request = _setupRequest.CreateMockHttpRequest(episode);
+        ((IDictionary<string, object?>)_episode)[propertyName] = "";
+        var request = _setupRequest.CreateMockHttpRequest(_episode);
 
         // Act
         var response = await _functions.IngressEpisode(request);
 
         // Assert
         var result = Assert.IsType<BadRequestObjectResult>(response);
-        Assert.Equal("nhs_number is required", result.Value);
+        Assert.Equal($"{propertyName} is required", result.Value);
+        _mockEventGridPublisherClient.Verify(x => x.SendEventAsync(It.IsAny<CloudEvent>(), It.IsAny<CancellationToken>()), Times.Never());
+    }
+
+    [Theory]
+    [MemberData(nameof(RequiredPropertyNames))]
+    public async Task CreateEpisodeEvent_ShouldReturnBadRequest_WhenRequiredPropertyIsWhitespace(string propertyName)
+    {
+        // Arrange
+        ((IDictionary<string, object?>)_episode)[propertyName] = " ";
+        var request = _setupRequest.CreateMockHttpRequest(_episode);
+
+        // Act
+        var response = await _functions.IngressEpisode(request);
+
+        // Assert
+        var result = Assert.IsType<BadRequestObjectResult>(response);
+        Assert.Equal($"{propertyName} is required", result.Value);
         _mockEventGridPublisherClient.Verify(x => x.SendEventAsync(It.IsAny<CloudEvent>(), It.IsAny<CancellationToken>()), Times.Never());
     }
 
@@ -183,54 +164,6 @@ public class BSSelectFunctionsTests
         _mockEventGridPublisherClient.Verify(x => x.SendEventAsync(It.IsAny<CloudEvent>(), It.IsAny<CancellationToken>()), Times.Never());
     }
 
-    [Fact]
-    public async Task CreateEpisodeEvent_ShouldReturnBadRequest_WhenDateOfBirthIsMissing()
-    {
-        // Arrange
-        var episode = new
-        {
-            episode_id = "123",
-            nhs_number = "9990000000",
-            first_given_name = "Test",
-            family_name = "User",
-        };
-        var request = _setupRequest.CreateMockHttpRequest(episode);
-
-        // Act
-        var response = await _functions.IngressEpisode(request);
-
-        // Assert
-        var result = Assert.IsType<BadRequestObjectResult>(response);
-        Assert.Equal("date_of_birth is required", result.Value);
-        _mockEventGridPublisherClient.Verify(x => x.SendEventAsync(It.IsAny<CloudEvent>(), It.IsAny<CancellationToken>()), Times.Never());
-    }
-
-    [Theory]
-    [InlineData(null)]
-    [InlineData("")]
-    [InlineData(" ")]
-    public async Task CreateEpisodeEvent_ShouldReturnBadRequest_WhenDateOfBirthIsEmptyValue(string? dateOfBirth)
-    {
-        // Arrange
-        var episode = new
-        {
-            episode_id = "123",
-            nhs_number = "9990000000",
-            date_of_birth = dateOfBirth,
-            first_given_name = "Test",
-            family_name = "User",
-        };
-        var request = _setupRequest.CreateMockHttpRequest(episode);
-
-        // Act
-        var response = await _functions.IngressEpisode(request);
-
-        // Assert
-        var result = Assert.IsType<BadRequestObjectResult>(response);
-        Assert.Equal("date_of_birth is required", result.Value);
-        _mockEventGridPublisherClient.Verify(x => x.SendEventAsync(It.IsAny<CloudEvent>(), It.IsAny<CancellationToken>()), Times.Never());
-    }
-
     [Theory]
     [InlineData("ABC")]
     [InlineData("123")]
@@ -257,114 +190,10 @@ public class BSSelectFunctionsTests
     }
 
     [Fact]
-    public async Task CreateEpisodeEvent_ShouldReturnBadRequest_WhenFirstGivenNameIsMissing()
-    {
-        // Arrange
-        var episode = new
-        {
-            episode_id = "123",
-            nhs_number = "9990000000",
-            date_of_birth = "1970-01-01",
-            family_name = "User",
-        };
-        var request = _setupRequest.CreateMockHttpRequest(episode);
-
-        // Act
-        var response = await _functions.IngressEpisode(request);
-
-        // Assert
-        var result = Assert.IsType<BadRequestObjectResult>(response);
-        Assert.Equal("first_given_name is required", result.Value);
-        _mockEventGridPublisherClient.Verify(x => x.SendEventAsync(It.IsAny<CloudEvent>(), It.IsAny<CancellationToken>()), Times.Never());
-    }
-
-    [Theory]
-    [InlineData(null)]
-    [InlineData("")]
-    [InlineData(" ")]
-    public async Task CreateEpisodeEvent_ShouldReturnBadRequest_WhenFirstGivenNameIsEmptyValue(string? firstGivenName)
-    {
-        // Arrange
-        var episode = new
-        {
-            episode_id = "123",
-            nhs_number = "9990000000",
-            date_of_birth = "1970-01-01",
-            first_given_name = firstGivenName,
-            family_name = "User",
-        };
-        var request = _setupRequest.CreateMockHttpRequest(episode);
-
-        // Act
-        var response = await _functions.IngressEpisode(request);
-
-        // Assert
-        var result = Assert.IsType<BadRequestObjectResult>(response);
-        Assert.Equal("first_given_name is required", result.Value);
-        _mockEventGridPublisherClient.Verify(x => x.SendEventAsync(It.IsAny<CloudEvent>(), It.IsAny<CancellationToken>()), Times.Never());
-    }
-
-    [Fact]
-    public async Task CreateEpisodeEvent_ShouldReturnBadRequest_WhenFamilyNameIsMissing()
-    {
-        // Arrange
-        var episode = new
-        {
-            episode_id = "123",
-            nhs_number = "9990000000",
-            date_of_birth = "1970-01-01",
-            first_given_name = "Test"
-        };
-        var request = _setupRequest.CreateMockHttpRequest(episode);
-
-        // Act
-        var response = await _functions.IngressEpisode(request);
-
-        // Assert
-        var result = Assert.IsType<BadRequestObjectResult>(response);
-        Assert.Equal("family_name is required", result.Value);
-        _mockEventGridPublisherClient.Verify(x => x.SendEventAsync(It.IsAny<CloudEvent>(), It.IsAny<CancellationToken>()), Times.Never());
-    }
-
-    [Theory]
-    [InlineData(null)]
-    [InlineData("")]
-    [InlineData(" ")]
-    public async Task CreateEpisodeEvent_ShouldReturnBadRequest_WhenFamilyNameIsEmptyValue(string? familyName)
-    {
-        // Arrange
-        var episode = new
-        {
-            episode_id = "123",
-            nhs_number = "9990000000",
-            date_of_birth = "1970-01-01",
-            first_given_name = "Test",
-            family_name = familyName,
-        };
-        var request = _setupRequest.CreateMockHttpRequest(episode);
-
-        // Act
-        var response = await _functions.IngressEpisode(request);
-
-        // Assert
-        var result = Assert.IsType<BadRequestObjectResult>(response);
-        Assert.Equal("family_name is required", result.Value);
-        _mockEventGridPublisherClient.Verify(x => x.SendEventAsync(It.IsAny<CloudEvent>(), It.IsAny<CancellationToken>()), Times.Never());
-    }
-
-    [Fact]
     public async Task CreateEpisodeEvent_ShouldReturnInternalServerError_WhenEventFailsToSend()
     {
         // Arrange
-        var episode = new
-        {
-            episode_id = "123",
-            nhs_number = "9990000000",
-            date_of_birth = "1970-01-01",
-            first_given_name = "Test",
-            family_name = "User",
-        };
-        var request = _setupRequest.CreateMockHttpRequest(episode);
+        var request = _setupRequest.CreateMockHttpRequest(_episode);
         var mockResponse = Mock.Of<Response>(r => r.IsError == true);
         _mockEventGridPublisherClient.Setup(x => x.SendEventAsync(It.IsAny<CloudEvent>(), It.IsAny<CancellationToken>())).ReturnsAsync(mockResponse);
 
@@ -381,15 +210,7 @@ public class BSSelectFunctionsTests
     public async Task CreateEpisodeEvent_ShouldReturnInternalServerError_WhenSendEventThrowsException()
     {
         // Arrange
-        var episode = new
-        {
-            episode_id = "123",
-            nhs_number = "9990000000",
-            date_of_birth = "1970-01-01",
-            first_given_name = "Test",
-            family_name = "User",
-        };
-        var request = _setupRequest.CreateMockHttpRequest(episode);
+        var request = _setupRequest.CreateMockHttpRequest(_episode);
         var mockResponse = Mock.Of<Response>(r => r.IsError == true);
         _mockEventGridPublisherClient.Setup(x => x.SendEventAsync(It.IsAny<CloudEvent>(), It.IsAny<CancellationToken>()))
             .ThrowsAsync(new RequestFailedException("Failed to send event to Event Grid"));
