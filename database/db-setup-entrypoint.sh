fabcd19ad47fe8a3074212a6e2bab0cf1ee424e6
warren <warren.pitterson1@nhs.net>
2025-05-08 17:27:13 +0100

    feat: db container running

--- database/db-setup-entrypoint.sh
+++ database/db-setup-entrypoint.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 echo "🔧 Starting DB setup..."
+
+# Verify that the script is executable
+if [ ! -x /database/db-setup-entrypoint.sh ]; then
+  echo "❌ Entry point script is not executable"
+  exit 1
+fi
+
+# Verify the content of the script is present and not empty
+echo "📄 Content of the script:"
+cat /database/db-setup-entrypoint.sh
+
+# Check if the SQL script exists
+if [ ! -f /database/create_database_statement.sql ]; then
+  echo "❌ SQL script not found!"
+  exit 1
+fi
+
 echo "📡 Connecting to SQL Server at db..."
 until /opt/mssql-tools/bin/sqlcmd -S db -U SA -P "${DATABASE_PASSWORD}" -Q "SELECT 1;" > /dev/null 2>&1
 do
@@ -7,5 +24,8 @@ do
     sleep 5
 done
 echo "✅ SQL Server is ready."
-/opt/mssql-tools/bin/sqlcmd -S db -U SA -P "${DATABASE_PASSWORD}" -d master -i create_database_statement.sql
+
+# Run the SQL setup script to create the database
+/opt/mssql-tools/bin/sqlcmd -S db -U SA -P "${DATABASE_PASSWORD}" -d master -i /database/create_database_statement.sql
+
 echo "✅ Database setup completed."
