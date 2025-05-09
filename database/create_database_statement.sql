ade6dcb140bb7717f66a92e7f95665d53a0cb511
warren <warren.pitterson1@nhs.net>
2025-05-09 15:06:48 +0100

    fix: database creation, create statement exit fix

--- database/create_database_statement.sql
+++ database/create_database_statement.sql
@@ -1,14 +1,23 @@
 USE master;
 GO
 
+-- Check if database exists and create it if it doesn't
 IF NOT EXISTS (
         SELECT name
         FROM sys.databases
         WHERE name = N'ServiceLayer'
         )
-    CREATE DATABASE [Test];
+BEGIN
+    CREATE DATABASE [ServiceLayer];
+END
 GO
 
-IF SERVERPROPERTY('ProductVersion') > '12'
+USE [ServiceLayer];
+GO
+
+-- Only try to set QUERY_STORE if SQL Server version is higher than 12 (SQL 2014)
+IF CAST(SERVERPROPERTY('ProductVersion') AS NVARCHAR(128)) > '12'
+BEGIN
     ALTER DATABASE [ServiceLayer] SET QUERY_STORE = ON;
+END
 GO
