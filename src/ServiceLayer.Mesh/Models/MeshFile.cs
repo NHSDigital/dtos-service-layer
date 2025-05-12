a4fb3bc9d733bb025863a2bb497dcd23fc2da636
alex-clayton-1 <alexander.clayton1@nhs.net>
2025-05-12 12:34:52 +0100

    feat: Set up DesignTimeDbContextFactory and created initial migration

--- src/ServiceLayer.Mesh/Models/MeshFile.cs
+++ src/ServiceLayer.Mesh/Models/MeshFile.cs
@@ -1,13 +1,19 @@
+using System.ComponentModel.DataAnnotations;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace ServiceLayer.Mesh.Models;
 
 public class MeshFile
 {
+    [MaxLength(255)]
     public required string FileId { get; set; }
+    [MaxLength(50)]
     public required MeshFileType FileType { get; set; }
+    [MaxLength(50)]
     public required string MailboxId { get; set; }
+    [MaxLength(20)]
     public required MeshFileStatus Status { get; set; }
+    [MaxLength(1024)]
     public string? BlobPath { get; set; }
     public DateTime FirstSeenUtc { get; set; }
     public DateTime LastUpdatedUtc { get; set; }
