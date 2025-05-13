29ea6ff131788e1aa5aa95f9377428b8b8cef750
Tyrrellion <samuel.tyrell1@nhs.net>
2025-05-13 11:27:13 +0100

    feat: adding Retry function
    Signed-off-by: Tyrrellion <samuel.tyrell1@nhs.net>

--- src/ServiceLayer.Mesh/Functions/FileRetryFunction.cs
+++ src/ServiceLayer.Mesh/Functions/FileRetryFunction.cs
@@ -1,6 +1,47 @@
-namespace ServiceLayer.Mesh.Functions;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using NHS.MESH.Client.Contracts.Services;
+using ServiceLayer.Mesh.Data;
+using ServiceLayer.Mesh.Messaging;
+using ServiceLayer.Mesh.Models;
 
-public class FileRetryFunction
+namespace ServiceLayer.Mesh.Functions
 {
-    
+    public class FileRetryFunction(
+        ILogger<FileRetryFunction> logger,
+        IMeshInboxService meshInboxService,
+        ServiceLayerDbContext serviceLayerDbContext,
+        IFileExtractQueueClient fileExtractQueueClient)
+    {
+        [Function("FileRetryFunction")]
+        public async Task Run([TimerTrigger("%FileRetryTimerExpression%")] TimerInfo myTimer)
+        {
+            var twelveHoursAgo = DateTime.UtcNow.AddHours(-12);
+
+            var files = await serviceLayerDbContext.MeshFiles
+                .Where(f =>
+                    (f.Status == MeshFileStatus.Discovered ||
+                     f.Status == MeshFileStatus.Extracting ||
+                     f.Status == MeshFileStatus.Extracted ||
+                     f.Status == MeshFileStatus.Transforming) && f.LastUpdatedUtc <= twelveHoursAgo)
+                .ToListAsync();
+
+            foreach (var file in files)
+            {
+                if (file.Status == MeshFileStatus.Discovered || file.Status == MeshFileStatus.Extracting)
+                {
+                    await fileExtractQueueClient.EnqueueFileExtractAsync(file);
+                    file.LastUpdatedUtc = DateTime.UtcNow;
+                    await serviceLayerDbContext.SaveChangesAsync();
+                }
+                else if (file.Status == MeshFileStatus.Extracted || file.Status == MeshFileStatus.Transforming)
+                {
+                    //await fileExtractQueueClient.EnqueueFileExtractAsync(file);
+                    file.LastUpdatedUtc = DateTime.UtcNow;
+                    await serviceLayerDbContext.SaveChangesAsync();
+                }
+            }
+        }
+    }
 }
