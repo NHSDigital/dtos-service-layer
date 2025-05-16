200229ca52f42455e30358b218ed1eae986e3649
alex-clayton-1 <alexander.clayton1@nhs.net>
2025-05-16 10:40:36 +0100

    feat: Started implementing File Transform Function

--- src/ServiceLayer.Mesh/Functions/FileTransformFunction.cs
+++ src/ServiceLayer.Mesh/Functions/FileTransformFunction.cs
@@ -1,8 +1,44 @@
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using ServiceLayer.Mesh.Data;
+using ServiceLayer.Mesh.Messaging;
+using ServiceLayer.Mesh.Models;
+using ServiceLayer.Mesh.Storage;
+
 namespace ServiceLayer.Mesh.Functions;
 
-public class FileTransformFunction
+// TODO - take dependency on IEnumerable<IFileTransformer>.
+// After initial common checks against database, find the appropriate implementation of IFileTransformer to handle the functionality that differs between file type.
+public class FileTransformFunction(
+    ILogger<FileTransformFunction> logger,
+    ServiceLayerDbContext serviceLayerDbContext,
+    IMeshFilesBlobStore meshFileBlobStore)
 {
+    [Function("FileTransformFunction")]
+    public async Task Run([QueueTrigger("%FileTransformQueueName%")] FileTransformQueueMessage message)
+    {
+        await using var transaction = await serviceLayerDbContext.Database.BeginTransactionAsync();
 
-    // TODO - take dependency on IEnumerable<IFileTransformer>.
-    // After initial common checks against database, find the appropriate implementation of IFileTransformer to handle the functionality that differs between file type.
+        var file = await serviceLayerDbContext.MeshFiles
+            .FirstOrDefaultAsync(f => f.FileId == message.FileId);
+
+        if (file == null)
+        {
+            logger.LogWarning("File with id: {fileId} not found in MeshFiles table.", message.FileId);
+            return;
+        }
+
+        if (file.Status != MeshFileStatus.Extracted)
+        {
+            logger.LogWarning("File with id: {fileId} found in MeshFiles table but is not suitable for transformation. Status: {status}", message.FileId, file.Status);
+            return;
+        }
+
+        file.Status = MeshFileStatus.Extracting;
+        file.LastUpdatedUtc = DateTime.UtcNow;
+        await serviceLayerDbContext.SaveChangesAsync();
+
+        await transaction.CommitAsync();
+    }
 }
