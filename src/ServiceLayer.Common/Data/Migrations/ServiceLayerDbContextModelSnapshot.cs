4683bbb2461138e481c1cb7976eda7200d7971ce
Ian Nelson <ian@iannelson.systems>
2025-05-19 08:57:52 +0100

    refactor: Split Data-related classes out into ServiceLayer.Common (#15)

--- src/ServiceLayer.Common/Data/Migrations/ServiceLayerDbContextModelSnapshot.cs
+++ src/ServiceLayer.Common/Data/Migrations/ServiceLayerDbContextModelSnapshot.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-using ServiceLayer.Mesh.Data;
+using ServiceLayer.Data;
 
 #nullable disable
 
