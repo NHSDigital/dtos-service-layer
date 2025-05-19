8ba4f3fe225a9bb564c808dc04b2e4074a75dd6d
alex-clayton-1 <alexander.clayton1@nhs.net>
2025-05-19 18:04:32 +0100

    feat: Implemented StagingPersister

--- src/ServiceLayer.Mesh/FileTypes/NbssAppointmentEvents/StagingPersister.cs
+++ src/ServiceLayer.Mesh/FileTypes/NbssAppointmentEvents/StagingPersister.cs
@@ -1,13 +1,59 @@
+using ServiceLayer.Data;
+using ServiceLayer.Data.Models;
 using ServiceLayer.Mesh.FileTypes.NbssAppointmentEvents.Models;
 
 namespace ServiceLayer.Mesh.FileTypes.NbssAppointmentEvents;
 
-// TODO - class to take validated AppointmentEventsFile and save the records to NbssAppointmentEvents table
-public class StagingPersister : IStagingPersister
+public class StagingPersister(ServiceLayerDbContext dbContext) : IStagingPersister
 {
-    public Task WriteStagedData(ParsedFile parsedFile)
+    public async Task WriteStagedData(ParsedFile parsedFile)
     {
-        // TODO - implement this
-        throw new NotImplementedException();
+        var nbssAppointmentEvents = MapFileDataRecordsToNbssAppointmentEvents(parsedFile);
+
+        await dbContext.NbssAppointmentEvents.AddRangeAsync(nbssAppointmentEvents);
+        await dbContext.SaveChangesAsync();
+    }
+
+    private static List<NbssAppointmentEvent> MapFileDataRecordsToNbssAppointmentEvents(ParsedFile parsedFile)
+    {
+        var events = new List<NbssAppointmentEvent>();
+
+        foreach (var record in parsedFile.DataRecords)
+        {
+            events.Add(new NbssAppointmentEvent
+            {
+                MeshFileId = "", // TODO - Get this from somewhere
+                BSO = record.Fields["BSO"],
+                ExtractId = parsedFile.FileHeader.ExtractId,
+                Sequence = record.Fields["Sequence"],
+                Action = record.Fields["Action"],
+                ClinicCode = record.Fields["Clinic Code"],
+                HoldingClinic = record.Fields["Holding Clinic"],
+                Status = record.Fields["Status"],
+                AttendedNotScreened = record.Fields["Attended Not Scr"],
+                AppointmenId = record.Fields["Appointment ID"],
+                NhsNumber = record.Fields["NHS Num"],
+                EpisodeType = record.Fields["Epsiode Type"],
+                EpisodeStart = DateOnly.ParseExact(record.Fields["Episode Start"], "yyyyMMdd"),
+                BatchId = record.Fields["BatchID"],
+                AppointmentType = record.Fields["Screen or Asses"],
+                ScreeningAppointmentNumber = byte.Parse(record.Fields["Screen Appt num"]),
+                BookedBy = record.Fields["Booked By"],
+                CancelledBy = record.Fields["Cancelled By"],
+                AppointmentDateTime = DateTime.ParseExact(record.Fields["Appt Date"] + record.Fields["Appt Time"], "yyyyMMddHHmm", null),
+                Location = record.Fields["Location"],
+                ClinicName = record.Fields["Clinic Name"],
+                ClinicNameOnLetters = record.Fields["Clinic Name (Let)"],
+                ClinicAddressLine1 = record.Fields["Clinic Address 1"],
+                ClinicAddressLine2 = record.Fields["Clinic Address 2"],
+                ClinicAddressLine3 = record.Fields["Clinic Address 3"],
+                ClinicAddressLine4 = record.Fields["Clinic Address 4"],
+                ClinicAddressLine5 = record.Fields["Clinic Address 5"],
+                ClinicPostcode = record.Fields["Postcode"],
+                ActionTimestamp = DateTime.ParseExact(record.Fields["Action Timestamp"], "yyyyMMdd-HHmmss", null)
+            });
+        }
+
+        return events;
     }
 }
