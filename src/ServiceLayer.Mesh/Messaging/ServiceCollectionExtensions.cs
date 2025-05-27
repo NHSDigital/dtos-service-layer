6edeb83049a86fa18bc43be13bcf19e65bcfa5b6
Ian Nelson <ian@iannelson.uk>
2025-05-27 16:41:24 +0100

    refactor: tidy up DI registration

---
+++ src/ServiceLayer.Mesh/Messaging/ServiceCollectionExtensions.cs
@@ -0,0 +1,35 @@
+using Azure.Identity;
+using Azure.Storage.Queues;
+using Microsoft.Extensions.DependencyInjection;
+using ServiceLayer.Common;
+
+namespace ServiceLayer.Mesh.Messaging;
+
+internal static class ServiceCollectionExtensions
+{
+    internal static IServiceCollection AddMessagingServices(this IServiceCollection services, bool isLocalEnvironment)
+    {
+        var queueClientOptions = new QueueClientOptions
+        {
+            MessageEncoding = QueueMessageEncoding.Base64
+        };
+
+        // Register QueueClients as singletons
+        services.AddSingleton(_ =>
+        {
+            if (isLocalEnvironment)
+            {
+                var connectionString = EnvironmentVariables.GetRequired("AzureWebJobsStorage");
+                return new QueueServiceClient(connectionString, queueClientOptions);
+            }
+
+            var meshStorageAccountUrl = EnvironmentVariables.GetRequired("MeshStorageAccountUrl");
+            return new QueueServiceClient(new Uri(meshStorageAccountUrl), new DefaultAzureCredential(), queueClientOptions);
+        });
+
+        services.AddSingleton<IFileExtractQueueClient, FileExtractQueueClient>();
+        services.AddSingleton<IFileTransformQueueClient, FileTransformQueueClient>();
+
+        return services;
+    }
+}
